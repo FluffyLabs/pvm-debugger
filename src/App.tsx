24a949c639bd5a83e8e27af034a4eb36af60dde7
Tomasz Drwięga <tomusdrw@gmail.com>
2024-07-25 12:52:11 +0200

    Change the way components are displayed.

--- src/App.tsx
+++ src/App.tsx
@@ -70,91 +70,114 @@ function App() {
 
   return (
     <>
-      <div className="grid w-full max-w-sm items-center gap-1.5">
-        <Label htmlFor="test-file">Load test from json file</Label>
-        <Input
-          id="test-file"
-          type="file"
-          accept="application/json"
-          onChange={(e) => {
-            if (e.target.files?.length) {
-              handleProgramUpload(e.target.files[0]);
-            }
-          }}
-        />
+      <div className="container py-3 text-left">
+        <div className="grid grid-cols-3 gap-1.5">
+          <div className="p-3 col-span-2 bg-sky-200 rounded-md">
+            <Label htmlFor="program">PVM program as array of numbers:</Label>
+            <Textarea
+              id="program"
+              placeholder="Paste program as an array of numbers"
+              value={programInput}
+              onChange={(e) => {
+                console.log(e.target.value);
+                try {
+                  setProgramInput(e.target.value);
+                  JSON.parse(e.target.value);
+                  setProgram(JSON.parse(e.target.value));
+                  setIsInvalidProgram(false);
+                } catch (e) {
+                  console.log("wrong json");
+                  setIsInvalidProgram(true);
+                }
+              }}
+            />
+            {isInvalidProgram && <div>Program is not a valid JSON array</div>}
+            <div className="text-right">
+              <Button className="my-2" onClick={handleClick}>
+                Check program
+              </Button>
+            </div>
+          </div>
+
+          <div className="p-3">
+            <Label htmlFor="test-file">Load test from json file:</Label>
+            <Input
+              id="test-file"
+              type="file"
+              accept="application/json"
+              onChange={(e) => {
+                if (e.target.files?.length) {
+                  handleProgramUpload(e.target.files[0]);
+                }
+              }}
+            />
+            <br />
+
+            <Label htmlFor="registers">Initial registers:</Label>
+            <Textarea
+              id="registers"
+              placeholder="Paste initial registers as an array of numbers"
+              value={registersInput}
+              onChange={(e) => {
+                console.log(e.target.value);
+                try {
+                  setRegistersInput(e.target.value);
+                  JSON.parse(e.target.value);
+                  setInitialState((prevState) => ({
+                    ...prevState,
+                    regs: JSON.parse(e.target.value),
+                  }));
+                  setIsInvalidRegisterTable(false);
+                } catch (e) {
+                  console.log("wrong json");
+                  setIsInvalidRegisterTable(true);
+                }
+              }}
+            />
+            {isInvalidRegisterTable && <div>Registers are not a valid JSON array</div>}
+          </div>
+        </div>
       </div>
 
-      <Textarea
-        placeholder="Paste initial registers as an array of numbers"
-        value={registersInput}
-        onChange={(e) => {
-          console.log(e.target.value);
-          try {
-            setRegistersInput(e.target.value);
-            JSON.parse(e.target.value);
-            setInitialState((prevState) => ({
-              ...prevState,
-              regs: JSON.parse(e.target.value),
-            }));
-            setIsInvalidRegisterTable(false);
-          } catch (e) {
-            console.log("wrong json");
-            setIsInvalidRegisterTable(true);
-          }
-        }}
-      />
-      {isInvalidRegisterTable && <div>Registers are not a valid JSON array</div>}
+      <div className="container py-3 font-mono">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Instruction Code</TableHead>
+              <TableHead>Instruction Name</TableHead>
+              <TableHead>Instruction Gas</TableHead>
+              <TableHead>Instruction Args</TableHead>
+              <TableHead>Instruction Errors</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {!!programPreviewResult?.length &&
+              programPreviewResult.map((programRow: any) => (
+                <TableRow>
+                  <TableCell>{programRow.instructionCode}</TableCell>
+                  <TableCell className="uppercase font-bold">{programRow.name}</TableCell>
+                  <TableCell>{programRow.gas}</TableCell>
+                  <TableCell className="text-xs text-left">
+                    <pre>{JSON.stringify(programRow.args, null, 2)}</pre>
+                  </TableCell>
+                  <TableCell>{programRow.error}</TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </div>
 
-      <Textarea
-        placeholder="Paste program as an array of numbers"
-        value={programInput}
-        onChange={(e) => {
-          console.log(e.target.value);
-          try {
-            setProgramInput(e.target.value);
-            JSON.parse(e.target.value);
-            setProgram(JSON.parse(e.target.value));
-            setIsInvalidProgram(false);
-          } catch (e) {
-            console.log("wrong json");
-            setIsInvalidProgram(true);
-          }
-        }}
-      />
-      {isInvalidProgram && <div>Program is not a valid JSON array</div>}
-      <Button onClick={handleClick}>Check program</Button>
+      <div className="container py-3 text-left text-xs">
+        <div className="grid grid-cols-2 p-3 bg-slate-100 rounded-md">
+          <pre className="p-3">
+            <code>Program preview: {JSON.stringify(programPreviewResult, null, 2)}</code>
+          </pre>
 
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Instruction Code</TableHead>
-            <TableHead>Instruction Name</TableHead>
-            <TableHead>Instruction Gas</TableHead>
-            <TableHead>Instruction Args</TableHead>
-            <TableHead>Instruction Errors</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {!!programPreviewResult?.length &&
-            programPreviewResult.map((programRow: any) => (
-              <TableRow>
-                <TableCell>{programRow.instructionCode}</TableCell>
-                <TableCell>{programRow.name}</TableCell>
-                <TableCell>{programRow.gas}</TableCell>
-                <TableCell>{JSON.stringify(programRow.args)}</TableCell>
-                <TableCell>{programRow.error}</TableCell>
-              </TableRow>
-            ))}
-        </TableBody>
-      </Table>
-
-      <pre>
-        <code>Program preview: {JSON.stringify(programPreviewResult)}</code>
-      </pre>
-
-      <pre>
-        <code>Program run result: {JSON.stringify(programRunResult)}</code>
-      </pre>
+          <pre className="p-3">
+            <code>Program run result: {JSON.stringify(programRunResult, null, 2)}</code>
+          </pre>
+        </div>
+      </div>
     </>
   );
 }
