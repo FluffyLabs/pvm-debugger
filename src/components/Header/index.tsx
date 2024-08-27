1944570d12a5728a15df30586d307d5ae3a54aa4
Wojciech Kwiatek <wojtek.kwiatek@gmail.com>
2024-08-27 20:00:10 +0200

    Add GitHub link (#96)
    * Add link to GitHub repo
    
    * Check only ts(x) files with prettier check

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,25 @@
 import Logo from "@/assets/logo.png";
+import GithubLogo from "@/assets/github-mark-white.svg";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip.tsx";
 
 export const Header = () => {
   return (
     <div className="bg-[#55B3F3] w-full flex flex-row items-center justify-between p-1 h-16 overflow-hidden">
-      <img src={Logo} className="w-[70px] ml-3" />
-      <div className="mr-3 text-white">
+      <img src={Logo} alt="FluffyLabs logo" className="w-[70px] ml-3" />
+      <div className="mr-3 text-white flex flex-row items-center justify-center gap-5">
         <pre>PVM debugger</pre>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <a target="_blank" href="https://github.com/fluffylabs/typeberry-toolkit" rel="noreferrer">
+                <img src={GithubLogo} alt="GitHub logo" width={32} />
+              </a>
+            </TooltipTrigger>
+            <TooltipContent side="left">
+              <p>Report an issue or fork on Github</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </div>
     </div>
   );
