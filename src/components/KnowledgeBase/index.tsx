574eef3c3c19c39e30e548f84fc3180ac5946055
Wojciech Kwiatek <wojtek.kwiatek@gmail.com>
2024-10-29 20:53:41 +0100

    Unify cards height

--- src/components/KnowledgeBase/index.tsx
+++ src/components/KnowledgeBase/index.tsx
@@ -25,7 +25,7 @@ export const KnowledgeBase = ({ currentInstruction }: { currentInstruction: Curr
   }, [searchText]);
 
   return (
-    <div className="border-2 rounded-md min-h-[450px] h-[70vh] p-3">
+    <div className="border-2 rounded-md overflow-auto h-[70vh] p-3">
       <div className="flex w-full items-center space-x-2 mb-3">
         <Input
           type="text"
