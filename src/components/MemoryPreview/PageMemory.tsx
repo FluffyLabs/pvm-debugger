b530533fa821e031324c1098cd00494bad79787e
Krystian Fras <krystian@fras.me>
2024-10-29 11:10:24 +0100

    Memory page refactor

--- src/components/MemoryPreview/PageMemory.tsx
+++ src/components/MemoryPreview/PageMemory.tsx
@@ -2,26 +2,39 @@ import { Input } from "@/components/ui/input";
 import { chunk } from "lodash";
 import { useSelector } from "react-redux";
 import { selectMemoryForFirstWorker } from "@/store/workers/workersSlice.ts";
+import { valueToNumeralSystem } from "../Instructions/utils";
+import { useContext } from "react";
+import { NumeralSystemContext } from "@/context/NumeralSystemProvider";
+import { NumeralSystem } from "@/context/NumeralSystem";
 
 const SPLIT_STEP = 8 as const;
-const toMemoryPageTabData = (memoryPage: Uint8Array | undefined, addressStart: number) => {
+const toMemoryPageTabData = (
+  memoryPage: Uint8Array | undefined,
+  addressStart: number,
+  numeralSystem: NumeralSystem,
+) => {
   return chunk(memoryPage || [], SPLIT_STEP).map((chunk, index) => {
     return {
       address: (index * SPLIT_STEP + addressStart).toString().padStart(6, "0"),
-      bits: chunk.map((byte) => byte.toString().padStart(3, "0")),
+      bytes: chunk.map((byte) => valueToNumeralSystem(byte, numeralSystem, numeralSystem ? 2 : 3)),
     };
   });
 };
 
 export const MemoryTable = ({ data, addressStart }: { addressStart: number; data: Uint8Array | undefined }) => {
-  const tableData = toMemoryPageTabData(data, addressStart);
+  const { numeralSystem } = useContext(NumeralSystemContext);
+  const tableData = toMemoryPageTabData(data, addressStart, numeralSystem);
 
   return (
     <div className="mt-5 max-h-[calc(70vh-150px)] overflow-y-auto">
-      {tableData.map(({ address, bits }) => (
+      {tableData.map(({ address, bytes }) => (
         <div className="grid grid-cols-3" key={address}>
           <div className="opacity-40">{address}</div>
-          <div className="col-span-2 font-semibold">{bits.join(" ")}</div>
+          <div className="col-span-2 font-semibold">
+            {bytes.map((byte, index) => (
+              <span className={`mr-[1px] ${index % 2 === 0 ? "text-gray-500" : "text-black"}`}>{byte}</span>
+            ))}
+          </div>
         </div>
       ))}
     </div>
