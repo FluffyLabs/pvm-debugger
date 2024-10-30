ca7b42539a2bd5c578346a12488eaaf8f520cbc6
Wojciech Kwiatek <wojtek.kwiatek@gmail.com>
2024-10-30 22:59:15 +0100

    Highlight multiple intructions (for each PVM) (#164)
    * Highlight multiple intructions (for each PVM)
    
    * Add tooltip over the instruction for discrepancy in program counter
    
    * Scroll to the instruction with the highest program counter automatically

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const hexToRgb = (hex: string) => {
+  const bigint = parseInt(hex.slice(1), 16);
+  const r = (bigint >> 16) & 255;
+  const g = (bigint >> 8) & 255;
+  const b = bigint & 255;
+  return `${r}, ${g}, ${b}`;
+};
