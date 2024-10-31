2cc348e80518f3e93386d56cad6492c56d3cf530
Krystian Fras <krystian@fras.me>
2024-10-31 12:30:55 +0100

    Add error handling for input

--- src/components/ProgramTextLoader/index.tsx
+++ src/components/ProgramTextLoader/index.tsx
@@ -2,6 +2,7 @@ import { Textarea } from "@/components/ui/textarea.tsx";
 import React, { useEffect, useState } from "react";
 import classNames from "classnames";
 import { bytes } from "@typeberry/block";
+import { logger } from "@/utils/loggerService";
 
 export const ProgramTextLoader = ({
   program,
@@ -11,7 +12,7 @@ export const ProgramTextLoader = ({
   setProgram: (val?: number[]) => void;
 }) => {
   const [programInput, setProgramInput] = useState(program?.length ? JSON.stringify(program) : "");
-  const [isInvalidProgram, setIsInvalidProgram] = useState(false);
+  const [error, setError] = useState<string>();
   useEffect(() => {
     setProgramInput(program?.length ? JSON.stringify(program) : "");
   }, [program]);
@@ -22,44 +23,44 @@ export const ProgramTextLoader = ({
 
     if (!newInput.startsWith("[")) {
       try {
-        setIsInvalidProgram(false);
+        setError(undefined);
         const parsedBlob = bytes.BytesBlob.parseBlob(newInput);
         setProgram(Array.prototype.slice.call(parsedBlob.buffer));
-      } catch (e) {
-        console.warn(e);
-        console.error("wrong binary file");
-        setIsInvalidProgram(true);
+      } catch (error) {
+        logger.error("Wrong binary file", { error, hideToast: true });
+        setError("Wrong binary file");
       }
     } else {
       try {
         JSON.parse(newInput);
         setProgram(JSON.parse(newInput));
-        setIsInvalidProgram(false);
-      } catch (e) {
-        // TODO only validate on submit
-        console.error("wrong json");
-        setIsInvalidProgram(true);
-        setProgram();
+        setError(undefined);
+      } catch (error) {
+        logger.error("Wrong JSON", { error, hideToast: true });
+        setError("Wrong JSON");
       }
     }
   };
 
   return (
-    <div
-      className={classNames("flex-auto flex gap-1 flex-col border-2 rounded-md ", {
-        "border-red-500": isInvalidProgram,
-      })}
-    >
-      <Textarea
-        autoFocus
-        className={classNames("w-full flex-auto font-mono border-0 text-base", {
-          "focus-visible:ring-3 focus-visible:outline-none active:outline-none": isInvalidProgram,
+    <div className="h-full">
+      <div
+        className={classNames("h-full flex-auto flex gap-1 flex-col border-2 rounded-md ", {
+          "border-red-500": !!error,
         })}
-        id="program"
-        placeholder="Paste the program as an array of numbers or hex string"
-        value={programInput}
-        onChange={handleOnChange}
-      />
+      >
+        <Textarea
+          autoFocus
+          className={classNames("w-full flex-auto font-mono border-0 text-base", {
+            "focus-visible:ring-3 focus-visible:outline-none active:outline-none": !!error,
+          })}
+          id="program"
+          placeholder="Paste the program as an array of numbers or hex string"
+          value={programInput}
+          onChange={handleOnChange}
+        />
+      </div>
+      {error && <p className="text-red-500">{error}</p>}
     </div>
   );
 };
