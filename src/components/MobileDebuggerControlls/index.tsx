3e30c6aac903061a705bd5e0f35ca501827d1544
Krystian Fras <krystian@fras.me>
2025-02-23 22:29:27 +0100

    disable drawer

--- src/components/MobileDebuggerControlls/index.tsx
+++ src/components/MobileDebuggerControlls/index.tsx
@@ -1,11 +1,11 @@
 import { DebuggerControlls } from "../DebuggerControlls";
 import { NumeralSystemSwitch } from "../NumeralSystemSwitch";
-import { ControllsDrawer } from "./ControllsDrawer";
+// import { ControllsDrawer } from "./ControllsDrawer";
 
 export const MobileDebuggerControls = () => {
   return (
     <div>
-      <ControllsDrawer />
+      {/* <ControllsDrawer /> */}
       <div className="flex items-center bg-[#F6F6F6] p-2">
         <DebuggerControlls />
         <NumeralSystemSwitch className="" />
