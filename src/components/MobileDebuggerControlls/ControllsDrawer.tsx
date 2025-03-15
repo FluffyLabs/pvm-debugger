fde76dc2082817ba5222ba8a6c898fc867eedbee
Krystian Fras <krystian@fras.me>
2025-03-15 20:44:12 +0100

    drawer-bg (#349)

--- src/components/MobileDebuggerControlls/ControllsDrawer.tsx
+++ src/components/MobileDebuggerControlls/ControllsDrawer.tsx
@@ -162,7 +162,7 @@ export const ControllsDrawer = (props: {
 
   return (
     <motion.div
-      className="absolute bottom-full left-0 w-full max-w-screen border-t"
+      className="absolute bottom-full left-0 w-full max-w-screen border-t bg-background"
       initial={{ height: minHeight }}
       animate={{ height: isOpen ? maxHeight : minHeight }}
       dragControls={dragControls}
